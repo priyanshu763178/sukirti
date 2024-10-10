c9d8ddb43f794ce3ccd72f0dd6a6695ee03f5405
Priyanshu kumar <priyanshukumar805132@gmail.com>
2024-10-10 18:10:09 +0530

    refactor-login-code

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -90,10 +90,10 @@ router.post('/register', async (req, res) => {
         return res.status(400).json({ message: 'Invalid credentials' });
       }
   
-    //   const isMatch = await bcrypt.compare(password, user.password);
-    //   if (!isMatch) {
-    //     return res.status(400).json({ message: 'Invalid credentials' });
-    //   }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: 'Invalid credentials' });
+      }
   
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
   
--- models/User.js
+++ models/User.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  password:{type:String,required:true}
+
 });
 
 const User = mongoose.model('User', UserSchema);
